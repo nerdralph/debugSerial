db9136a281b14a983b4beadf2beb605c70902ad2
Nerd Ralph <ralphdoncaster@gmail.com>
2020-04-14 11:10:17 -0300

    cleanup example and add comments

--- examples/print/print.ino
+++ examples/print/print.ino
@@ -1,5 +1,13 @@
-// 9.2M/(7+2) = 1.022Mbps
-#define DTXWAIT 2
+/* debugSerial example
+ * Ralph Doncaster (nerdralph) MIT Licence open source
+ */
+
+// default extra wait = 1 cycle (8 cycles/bit total)
+// valid range = 0-3 cycles, for a total of 7-10 cycles/bit 
+// #define DTXWAIT 1
+
+// default TX GPIO = PB0
+// #define DBG_TX B,0 
 
 #include <debugSerial.h>
 
@@ -9,8 +17,12 @@ void setup() {}
 
 void loop()
 {
-    dSerial.println( F("\nHi ") );
-    dSerial.println(42);
-    dSerial.println(42, HEX);
-    delay(1000);
+    uint8_t count = 0;
+    while (1) {
+        dSerial.print( F("count: ") );
+        dSerial.print(count);
+        dSerial.print( F(" 0x") );
+        dSerial.println(count++, HEX);
+        delay(1000);
+    }
 }
